5e73acfdd1d5f072474cb97d2dcb40b11172ec8c
Juliana <julianacristina030@gmail.com>
2024-07-10 14:46:50 -0300

    First class position

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
 
     }
 }
